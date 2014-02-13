fe270d0a80d48d4f48e3da7c5a3c6b6f01df5758
bronhuston <bronhuston@gmail.com>
2014-02-13 01:04:47 -0800

    Added Files from Mantis

---
+++ src/beetle/Game.java
@@ -0,0 +1,21 @@
+package beetle;
+import java.awt.*;
+import java.util.*;
+import java.io.*;
+
+public class Game{
+	private int[] score = {0,0};
+	public Game(){}
+    /**
+        increaseScore takes an index i
+        and adds 1 the array score.
+        i = 0 -> player score
+        i = 1 -> computer score
+    */
+	public void increaseScore(int i){
+        this.score[i]++;
+    }
+    public int getScore(int i){
+		return score[i];
+}
+}
