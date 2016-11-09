f56042987de31767051900d99afdd2ed2e9b2828
Ricky Yuanqi Li <yuanqili@umail.ucsb.edu>
2016-11-09 12:54:43 -0800

    totally rewrite the program

--- src/edu/ucsb/cs56/projects/games/beetle/player/Ant.java
+++ src/edu/ucsb/cs56/projects/games/beetle/player/Ant.java
@@ -1,11 +1,6 @@
 package edu.ucsb.cs56.projects.games.beetle.player;
 
 public class Ant extends Player {
-    public Ant() {
-        this.partNeed = new int[] {1, 1, 6, 2, 2, 1};
-        this.partPre  = new int[] {-1, 0, 0, 1, 1, 0};
-        this.partName = new String[] {"thorax", "head", "leg", "eye", "antenna", "abdomen"};
-    }
     public Ant(String name) {
         this.name = name;
         this.partNeed = new int[] {1, 1, 6, 2, 2, 1};
