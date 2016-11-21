41a340d1554f44c555890f5543564a3b00606427
samarth10 <samarth@umail.ucsb.edu>
2016-11-20 20:15:31 -0800

    Update Ladybug.java

--- src/edu/ucsb/cs56/projects/games/beetle/player/Ladybug.java
+++ src/edu/ucsb/cs56/projects/games/beetle/player/Ladybug.java
@@ -1,5 +1,12 @@
 package edu.ucsb.cs56.projects.games.beetle.player;
 
+/** 
+   * The ladybug class extends Player for the game.
+   * @version Fall 2016, CS56, UCSB
+   * @author Yuanqi Li
+   * @author Samarth Goyal  
+*/
+
 public class Ladybug extends Player{
 	public Ladybug(String name) {
 		this.name = name;
